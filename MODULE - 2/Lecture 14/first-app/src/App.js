3879cdecde2f510691fb976a6685c7dd59cfaa1a
Aditya Soni <soni96996@gmail.com>
2024-07-05 01:40:13 +0530

    update

--- MODULE - 2/Lecture 14/first-app/src/App.js
+++ MODULE - 2/Lecture 14/first-app/src/App.js
@@ -2,23 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const NAME = "CipherSchools";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1 style={{
+          color: "red",
+          textAlign: "center",
+        }}>
+          hello from {NAME}!
+        </h1>
+      </div>
+      <div>
+        <p>This is Cipher Schools</p>
+      </div>
+    </>
   );
 }
 
