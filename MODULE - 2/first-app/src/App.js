99f76257f8bc921eb50950090d3b089ef7888f5e
Aditya Soni <soni96996@gmail.com>
2024-07-06 00:28:08 +0530

    update

--- MODULE - 2/first-app/src/App.js
+++ MODULE - 2/first-app/src/App.js
@@ -1,45 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from "react";
-
-// function App() {
-//   const NAME = "CipherSchools";
-//   return (
-//     <>
-//       <div>
-//         <h1 style={{
-//           color: "red",
-//           textAlign: "center",
-//         }}>
-//           hello from {NAME}!
-//         </h1>
-//       </div>
-
-//       <div>
-//         <p>This is Cipher Schools</p>
-//       </div>
-//     </>
-//   );
-// }
+import ToDoScreen from "./screens/ToDoScreen";
 
 class App extends Component {
-  name = "CipherSchools";
-
-  render() {
-    return (
-      <>
-        <div>
-          <h1>This is {this.name}</h1>
-        </div>
-
-        <div>
-          <p>This is paragraph and name is : {this.name}!</p>
-        </div>
-      </>
-    );
-  }
-
+    render() {
+        return <ToDoScreen />;
+    };
 }
 
 export default App;
-
