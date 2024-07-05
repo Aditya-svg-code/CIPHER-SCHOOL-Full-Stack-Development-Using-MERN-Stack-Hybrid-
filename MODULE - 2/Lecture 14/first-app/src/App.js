37ed787055ca63ac5eae4c21a938a17d3e195b68
Aditya Soni <soni96996@gmail.com>
2024-07-05 23:22:26 +0530

    update

--- MODULE - 2/Lecture 14/first-app/src/App.js
+++ MODULE - 2/Lecture 14/first-app/src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  const NAME = "CipherSchools";
-  return (
-    <>
-      <div>
-        <h1 style={{
-          color: "red",
-          textAlign: "center",
-        }}>
-          hello from {NAME}!
-        </h1>
-      </div>
-      <div>
-        <p>This is Cipher Schools</p>
-      </div>
-    </>
-  );
+// function App() {
+//   const NAME = "CipherSchools";
+//   return (
+//     <>
+//       <div>
+//         <h1 style={{
+//           color: "red",
+//           textAlign: "center",
+//         }}>
+//           hello from {NAME}!
+//         </h1>
+//       </div>
+
+//       <div>
+//         <p>This is Cipher Schools</p>
+//       </div>
+//     </>
+//   );
+// }
+
+class App extends Component {
+  name = "CipherSchools";
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>This is {this.name}</h1>
+        </div>
+
+        <div>
+          <p>This is paragraph and name is : {this.name}!</p>
+        </div>
+      </>
+    );
+  }
+
 }
 
 export default App;
