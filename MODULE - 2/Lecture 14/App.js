9a30f86f95b4990daedfe55a52d2bf1d2bd6538b
Aditya Soni <soni96996@gmail.com>
2024-07-06 00:05:04 +0530

    update

---
+++ MODULE - 2/Lecture 14/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from "react";
+
+function App() {
+    const NAME = "CipherSchools";
+    return (
+        <>
+            <div>
+                <h1 style={{
+                    color: "red",
+                    textAlign: "center",
+                }}>
+                    hello from {NAME}!
+                </h1>
+            </div>
+
+            <div>
+                <p>This is Cipher Schools</p>
+            </div>
+        </>
+    );
+}
+
+
+
+export default App;
+
