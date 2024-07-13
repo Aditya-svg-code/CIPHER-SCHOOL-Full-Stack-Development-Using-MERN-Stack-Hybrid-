9c4bd6a40adf336601139700638340fbfcb7daaf
Aditya Soni <soni96996@gmail.com>
2024-07-14 00:08:00 +0530

    updated

--- MODULE - 2/first-app/src/screens/ToDoScreen.js
+++ MODULE - 2/first-app/src/screens/ToDoScreen.js
@@ -1,8 +1,27 @@
 import { Component } from "react";
 
 class ToDoScreen extends Component {
+
+    state = {
+        taskCount: 0,
+    };
+
     render() {
-        return <h1 className="ui heading center">To Do List</h1>;
+        return (
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div>
+                    <button onClick={(e) => {
+                        this.setState({
+                            ...this.state,
+                            taskCount: this.state.taskCount + 1,
+                        });
+                        console.log(this.taskCount);
+                    }} className="ui secondary button">Add Task</button>
+                    <p>The number of Tasks are : {this.state.taskCount}</p>
+                </div>
+            </div>
+        );
     }
 }
 
