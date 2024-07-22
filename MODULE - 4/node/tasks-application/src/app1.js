ed4ef7d64f417258ed1e4bf0030fea9719d2bad7
Aditya Soni <soni96996@gmail.com>
2024-07-23 01:36:30 +0530

    update

--- MODULE - 4/node/tasks-application/src/app1.js
+++ MODULE - 4/node/tasks-application/src/app1.js
@@ -3,6 +3,8 @@ const express = require("express");
 const Task = require("./models/Task");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("This is some response from your node.js server");
 });
@@ -18,6 +20,33 @@ app.post("/add-task", async (req, res) => {
     await task.save();
     return res.status(201).send({ message: "Saved!" })
 });
+
+app.get("/get-tasks", async (req, res) => {
+    const taskList = await Task.find();
+    return res.status(200).send({ taskList });
+});
+
+app.put("/update-task/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const updateResult = await Task.updateOne({ _id: taskId },
+        {
+            $set: { ...req.body },
+        }
+    );
+    if (!updateResult.matchedCount) {
+        return res.status(404).send({ message: `Task with ID: ${taskId} was not found.` });
+    }
+    return res.status(200).send({ message: "Update Success" });
+});
+
+app.delete("/delete-task/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const deleteResult = await Task.deleteOne({ _id: taskId });
+    if (!deleteResult.deletedCount) {
+        return res.status(404).send({ message: `Task with ID: ${taskId} was not found.` });
+    }
+    return res.status(200).send({ message: "Delete Success" });
+})
 
 app.listen(3000, () => {
     console.log("Server is running.");
