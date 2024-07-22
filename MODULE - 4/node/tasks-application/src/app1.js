63b32e93581ba8beb128516f3179b425a03d49be
Aditya Soni <soni96996@gmail.com>
2024-07-23 01:19:32 +0530

    update

--- MODULE - 4/node/tasks-application/src/app1.js
+++ MODULE - 4/node/tasks-application/src/app1.js
@@ -1,4 +1,6 @@
+require("./appMongoose");
 const express = require("express");
+const Task = require("./models/Task");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -9,6 +11,12 @@ app.get("/add", (req, res) => {
     let { a: firstNumber, b: secondNumber } = req.query;
     let sum = parseInt(firstNumber) + parseInt(secondNumber);
     res.send({ sum });
+});
+
+app.post("/add-task", async (req, res) => {
+    const task = new Task({ title: "Test Task", description: "Test task desc" });
+    await task.save();
+    return res.status(201).send({ message: "Saved!" })
 });
 
 app.listen(3000, () => {
