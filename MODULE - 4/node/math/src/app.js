9df9342c78d89f3bce6b90fdbfd34474836a82af
Aditya Soni <soni96996@gmail.com>
2024-07-22 21:29:26 +0530

    update

--- MODULE - 4/node/math/src/app.js
+++ MODULE - 4/node/math/src/app.js
@@ -1,7 +1,18 @@
-const addNumbers = (...args) => {
-    let sum = 0;
-    args.forEach((arg) => (sum += arg));
-    return sum;
-};
+// const addNumbers = (...args) => {
+//     let sum = 0;
+//     args.forEach((arg) => (sum += arg));
+//     return sum;
+// };
 
-console.log(addNumbers(4, 5, 1, -2, 10, 5));
+// console.log(addNumbers(4, 5, 1, -2, 10, 5));
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.write("This is some response from your first Node.js server");
+    res.end();
+});
+
+server.listen(4000, () => {
+    console.log("Server is listening on port 4000");
+});
